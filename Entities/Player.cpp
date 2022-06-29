62e600b9cdfb3db7b9abfeaa8a2810394de10c05
MaksYus <84965322+MaksYus@users.noreply.github.com>
2022-06-29 10:30:51 +0500

    adding more animations

--- Entities/Player.cpp
+++ Entities/Player.cpp
@@ -10,7 +10,9 @@ Player::Player(float x, float y, sf::Texture& texture_sheet)
     this->createAnimationComponent(texture_sheet);
 
     this->animationComponent->addAnimation("IDLE_RIGHT",2.f,0,0, 8,0,64,64);
-    this->animationComponent->addAnimation("WALK_RIGHT",2.f,0,0, 8,0,64,64);
+    this->animationComponent->addAnimation("WALK_RIGHT",0.25,0,1, 5,1,64,64);
+    this->animationComponent->addAnimation("IDLE_LEFT",2.f,0,2, 8,2,64,64);
+    this->animationComponent->addAnimation("WALK_LEFT",0.25,0,3, 5,3,64,64);
 }
 
 Player::~Player()
@@ -29,8 +31,18 @@ void Player::initComponents(){
 void Player::update(const float&dt){
     this->movementComponent->update(dt);
 
-    if (this->movementComponent->idle())
-        this->animationComponent->play("IDLE_RIGHT", dt);
-    else
-        this->animationComponent->play("WALK_RIGHT", dt);
+    sf::Vector2i mousePos = sf::Mouse::getPosition();
+
+    if (this->movementComponent->idle()){
+        if(this->sprite.getPosition().x+32 < mousePos.x)
+            this->animationComponent->play("IDLE_RIGHT", dt);
+        else
+            this->animationComponent->play("IDLE_LEFT", dt);
+        }
+    else{
+        if(this->sprite.getPosition().x+32 < mousePos.x)
+            this->animationComponent->play("WALK_RIGHT", dt);
+        else
+            this->animationComponent->play("WALK_LEFT", dt);
+        }
 }
